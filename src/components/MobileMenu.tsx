de6e7a388293493cbc195fb602bb58be06796b97
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2023-06-12 17:44:52 -0600

    draft responsive nav

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,9 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { cssObj } from '@fuel-ui/css';
-import { Box, IconButton, Icon, Flex, FuelLogo } from '@fuel-ui/react';
+import {
+  Box,
+  IconButton,
+  Icon,
+  Flex,
+  FuelLogo,
+  Button,
+  Link,
+  Text,
+} from '@fuel-ui/react';
 import { AnimatePresence, motion } from 'framer-motion';
 import type { AnimationProps } from 'framer-motion';
 import { useEffect, useState } from 'react';
+import { LinkObject, NAVIGATION } from '../constants';
 
 import { Search } from './Search';
 import { Sidebar } from './Sidebar';
@@ -62,7 +72,23 @@ export function MobileMenu() {
         </Flex>
         {button}
       </Flex>
-      <Sidebar />
+      <Box css={styles.navContainer}>
+        <Flex css={styles.nav} direction={'column'}>
+          {NAVIGATION.map((item, index) => {
+            if (item.type === 'menu') {
+              return <MenuButton key={index} item={item} />;
+            } else if (item.type === 'internal-link') {
+              return (
+                <Button key={index} css={styles.navButton} variant={'link'}>
+                  {item.name}
+                </Button>
+              );
+            }
+          })}
+        </Flex>
+
+        <Sidebar />
+      </Box>
     </MotionBox>
   );
 
@@ -74,6 +100,45 @@ export function MobileMenu() {
         {showing && <Box css={styles.overlay}>{content}</Box>}
       </AnimatePresence>
     </Box>
+  );
+}
+
+function MenuButton({ item }: { item: LinkObject }) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  return (
+    <>
+      <Button
+        onPress={() => setIsOpen(!isOpen)}
+        css={styles.navButton}
+        variant={'outlined'}
+      >
+        {item.name}
+      </Button>
+      {isOpen && item.menu && (
+        <Flex direction={'column'} justify={'flex-start'}>
+          {item.menu.map((menuItem) => {
+            if (
+              menuItem.type === 'internal-link' ||
+              menuItem.type === 'external-link'
+            ) {
+              return (
+                <Link
+                  css={styles.menuLink}
+                  href={menuItem.link}
+                  isExternal={menuItem.type === 'external-link'}
+                >
+                  <Button css={styles.menuButton} variant={'link'}>
+                    {menuItem.name}
+                  </Button>
+                </Link>
+              );
+            } else if (menuItem.type === 'category') {
+              return <Text css={{ color: '$accent11' }}>{menuItem.name}</Text>;
+            }
+          })}
+        </Flex>
+      )}
+    </>
   );
 }
 
@@ -136,9 +201,37 @@ const styles = {
     width: '100vw',
     height: '100%',
     background: '$bodyColor',
-
     '@sm': {
       width: '400px',
     },
+  }),
+  navButton: cssObj({
+    borderRadius: '8px',
+  }),
+  menuButton: cssObj({
+    color: 'inherit',
+    '&:hover': {
+      textDecoration: 'none !important',
+    },
+  }),
+  menuLink: cssObj({
+    justifyContent: 'flex-start',
+    paddingLeft: '8px',
+    color: 'var(--colors-gray10)',
+    '&:hover': {
+      background: '$accent2',
+      color: '$accent11',
+      textDecoration: 'none',
+    },
+  }),
+  navContainer: cssObj({
+    overflow: 'auto',
+    maxHeight: 'calc(100vh - 100px)',
+  }),
+  nav: cssObj({
+    borderBottom: '2px solid $accent11',
+    paddingBottom: '$2',
+    marginBottom: '$4',
+    gap: '$2',
   }),
 };
