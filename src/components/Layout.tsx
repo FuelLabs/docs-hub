879886a6d913bf814b9a4b761e064654d19fbcbf
Pedro Nauck <pedronauck@gmail.com>
2023-07-07 17:48:43 -0300

    fix: styles

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -50,11 +50,10 @@ const styles = {
     maxWidth: '100vw',
     width: '100vw',
     height: '100vh',
-    display: 'grid',
-    gridTemplateColumns: '1fr',
     gridTemplateRows: '80px auto',
 
     '@xl': {
+      display: 'grid',
       gridTemplateColumns: '250px 1fr 220px',
       gridTemplateRows: '80px auto',
       gridColumnGap: '$24',
