0eb68da1d882a9c95f8f6e98832b8ea624ac700e
Pedro Nauck <pedronauck@gmail.com>
2023-07-11 20:55:15 -0300

    fix: shiki

--- src/lib/plugins/rehype-code.ts
+++ src/lib/plugins/rehype-code.ts
@@ -1,11 +1,64 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import * as fs from 'fs/promises';
 import { toText } from 'hast-util-to-text';
 import { h } from 'hastscript';
+import { join as pathJoin } from 'path';
 import type { Options as RehypeCodeOptions } from 'rehype-pretty-code';
 import rehypeCode from 'rehype-pretty-code';
 import type { Root } from 'remark-gfm';
+import { getHighlighter as shikiGetHighlighter } from 'shiki';
 import type { PluggableList } from 'unified';
 import { visit } from 'unist-util-visit';
+
+// Shiki loads languages and themes using "fs" instead of "import", so Next.js
+// doesn't bundle them into production build. To work around, we manually copy
+// them over to our source code (lib/shiki/*) and update the "paths".
+//
+// Note that they are only referenced on server side
+// See: https://github.com/shikijs/shiki/issues/138
+const getShikiPath = (): string => {
+  return pathJoin(process.cwd(), 'public/shiki');
+};
+
+const touched = { current: false };
+
+// "Touch" the shiki assets so that Vercel will include them in the production
+// bundle. This is required because shiki itself dynamically access these files,
+// so Vercel doesn't know about them by default
+const touchShikiPath = (): void => {
+  if (touched.current) return; // only need to do once
+  fs.readdir(getShikiPath()); // fire and forget
+  touched.current = true;
+};
+
+const getHighlighter: RehypeCodeOptions['getHighlighter'] = async (options) => {
+  touchShikiPath();
+
+  const highlighter = await shikiGetHighlighter({
+    // This is technically not compatible with shiki's interface but
+    // necessary for rehype-pretty-code to work
+    // - https://rehype-pretty-code.netlify.app/ (see Custom Highlighter)
+    ...(options as any),
+    langs: [
+      'rust',
+      'javascript',
+      'typescript',
+      'tsx',
+      'jsx',
+      'bash',
+      'shell',
+      'sh',
+      'json',
+      'toml',
+    ],
+    paths: {
+      languages: `${getShikiPath()}/languages/`,
+      themes: `${getShikiPath()}/themes/`,
+    },
+  });
+
+  return highlighter;
+};
 
 function codeImport() {
   return function transformer(tree: Root) {
@@ -134,6 +187,7 @@ function addRawCode() {
 
 const getRehypeCodeOptions = (): Partial<RehypeCodeOptions> => ({
   theme: 'dracula',
+  getHighlighter,
 });
 
 export const getMdxCode = (): PluggableList => [
