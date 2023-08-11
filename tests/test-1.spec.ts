cf7ae9b58853793a4bc0a71fb45934c208063a4a
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2023-08-11 14:04:22 -0600

    progress

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -3,20 +3,12 @@ import { execSync } from 'child_process';
 import fs from 'fs';
 import { EOL } from 'os';
 
+interface Instruction {
+  text: string;
+  output: string;
+}
+
 test('test dev quickstart', async ({ page, context }) => {
-  interface Instruction {
-    text: string;
-    output: string;
-  }
-
-  function print(title: string, instruction: Instruction) {
-    console.log(`---${title}---`);
-    console.log('TEXT: ', instruction.text);
-    if (instruction.output !== '') {
-      console.log('OUTPUT: ', instruction.output);
-    }
-  }
-
   async function clickCopyButton(id: string) {
     let clipboardText = { text: '', output: '' };
     const selector = `#${id} + div > div > div > button[aria-label="Copy to Clipboard"]`;
@@ -33,104 +25,83 @@ test('test dev quickstart', async ({ page, context }) => {
     return clipboardText;
   }
 
+  async function runCommand(buttonName: string, goToFolder?: string) {
+    const copied = await clickCopyButton(buttonName);
+    const command = goToFolder ? goToFolder + copied.text : copied.text;
+    const commandOutput = execSync(command, {
+      encoding: 'utf-8',
+    });
+    if (copied.output !== '') {
+      compareOutputs(commandOutput, copied.output);
+    }
+  }
+
+  async function writeToFile(buttonName: string, filePath: string) {
+    const content = await clickCopyButton(buttonName);
+    fs.writeFileSync(filePath, content.text + '\n\n');
+  }
+
+  async function appendToFile(buttonName: string, filePath: string) {
+    const content = await clickCopyButton(buttonName);
+    fs.appendFileSync(filePath, content.text + '\n\n');
+  }
+
+  async function compareToFile(buttonName: string, pathName: string) {
+    const expected = await clickCopyButton(buttonName);
+    const actual = fs.readFileSync(pathName, { encoding: 'utf8' });
+    compareOutputs(expected.text, actual);
+  }
+
   await page.goto(
     'http://localhost:3000/guides/quickstart/building-a-smart-contract/'
   );
   await context.grantPermissions(['clipboard-read', 'clipboard-write']);
 
+  // TODO: use this in the CI
   // get the default toolchain command
   // const setDefaultFuelupToolchain = await clickCopyButton(
   //   'set-default-fuelup-toolchain'
   // );
-  // print('DEFAULT TOOLCHAIN COMMAND', setDefaultFuelupToolchain);
-  // TODO: use this in the CI
+
+  setupFolders();
 
   // get the command to make a new folder
-  // const createProjectFolder = await clickCopyButton('create-project-folder');
-  // print('MAKE NEW DIR', createProjectFolder);
-
-  if (!fs.existsSync('guides-testing')) {
-    fs.mkdirSync('guides-testing');
-  }
-
-  // const goToTestingFolder = 'cd guides-testing && ';
+  const createProjectFolder = await clickCopyButton('create-project-folder');
+  const goToTestingFolder = 'cd guides-testing && ';
   const goToProjectFolder = 'cd guides-testing/fuel-project && ';
-  // // const goToContractFolder =
-  // //   'cd guides-testing/fuel-project/counter-contract && ';
+  const goToContractFolder =
+    'cd guides-testing/fuel-project/counter-contract && ';
 
-  // execSync(goToTestingFolder + createProjectFolder.text, {
-  //   encoding: 'utf-8',
-  // });
-
-  // // get the command to make a new forc project
-  // const createContract = await clickCopyButton('create-contract');
-  // // print('MAKE NEW CONTRACT', createContract);
-
-  // const createContractOutput = execSync(
-  //   goToTestingFolder + createContract.text,
-  //   {
-  //     encoding: 'utf-8',
-  //   }
-  // );
-  // if (createContract.output !== '') {
-  //   expect(createContractOutput === createContract.output).toBeTruthy();
-  // }
-
-  // get the command to show the contract tree
-  const contractTree = await clickCopyButton('contract-tree');
-  // print('TREE', contractTree);
-  const contractTreeCommand = goToProjectFolder + contractTree.text;
-  // console.log(contractTreeCommand);
-  const contractTreeOutput = execSync(contractTreeCommand, {
+  execSync(goToTestingFolder + createProjectFolder.text, {
     encoding: 'utf-8',
   });
 
-  const first = contractTreeOutput.trim();
-  const second = contractTree.output.trim();
-  const split1 = first.split(EOL);
-  const split2 = second.split(EOL);
-  expect(split1.length === split2.length).toBeTruthy();
-  split1.forEach((l, i) => {
-    expect(l.trim() === split2[i].trim()).toBeTruthy();
-  });
+  // get the command to make a new forc project
+  await runCommand('create-contract', goToTestingFolder);
+
+  // get the command to show the contract tree
+  await runCommand('contract-tree', goToProjectFolder);
 
   const CONTRACT_PATH =
     'guides-testing/fuel-project/counter-contract/src/main.sw';
-  // get part 1 of the contract
-  const contractPart1 = await clickCopyButton('program-type');
-  fs.writeFileSync(CONTRACT_PATH, contractPart1.text + '\n\n');
-  print('CONTRACT PART 1', contractPart1);
 
-  // // // get part 2 of the contract
-  // const contractPart2 = await clickCopyButton('storage');
-  // // // print('CONTRACT PART 2', contractPart2);
-  // fs.appendFileSync(CONTRACT_PATH, contractPart2.text + '\n\n');
+  // add part 1 to contract
+  await writeToFile('program-type', CONTRACT_PATH);
+  // add part 2 to contract
+  await appendToFile('storage', CONTRACT_PATH);
+  // add part 3 to contract
+  await appendToFile('abi', CONTRACT_PATH);
+  //  add part 4 to contract
+  await appendToFile('impl', CONTRACT_PATH);
 
-  // // get part 3 of the contract
-  // const contractPart3 = await clickCopyButton('abi');
-  // // print('CONTRACT PART 3', contractPart3);
-  // fs.appendFileSync(CONTRACT_PATH, contractPart3.text + '\n\n');
+  // check the whole contract
+  await compareToFile('entire-contract', CONTRACT_PATH);
 
-  // // get part 4 of the contract
-  // const contractPart4 = await clickCopyButton('impl');
-  // // print('CONTRACT PART 4', contractPart4);
-  // fs.appendFileSync(CONTRACT_PATH, contractPart4.text + '\n');
+  // // build the contract
+  // await runCommand('build-contract', goToContractFolder);
 
-  // // get the whole contract
-  // const entireContract = await clickCopyButton('entire-contract');
-  // const newContract = fs.readFileSync(CONTRACT_PATH, { encoding: 'utf8' });
-  // console.log('newContract', newContract);
-  // console.log('entireContract.text', entireContract.text);
-  // expect(newContract === entireContract.text).toBeTruthy();
-  // print('CONTRACT ALL', entireContract);
-
-  // // command to build the contract
-  // const buildCommand = await clickCopyButton('build-contract');
-  // print('BUILD COMMAND', buildCommand);
-
-  // // command to show the built contract tree
-  // const builtTree = await clickCopyButton('built-contract-tree');
-  // print('BUILT CONTRACT TREE', builtTree);
+  // // built contract tree
+  // await runCommand('built-contract-tree', goToContractFolder);
 
   // // command to show install cargo generate
   // const installCargoGen = await clickCopyButton('install-cargo-generate');
@@ -153,7 +124,17 @@ test('test dev quickstart', async ({ page, context }) => {
   // print('RUN TEST HARNESS', runCargoTest);
 });
 
-function separateCommand(text: string) {
+function setupFolders() {
+  if (!fs.existsSync('guides-testing')) {
+    fs.mkdirSync('guides-testing');
+  }
+
+  if (fs.existsSync('guides-testing/fuel-project')) {
+    fs.rmSync('guides-testing/fuel-project', { recursive: true, force: true });
+  }
+}
+
+function separateCommand(text: string): Instruction {
   const lines = text.trim().split('\n');
   // The first line contains the command
   const command = lines[0].replace('$', '').trim();
@@ -165,15 +146,13 @@ function separateCommand(text: string) {
   };
 }
 
-// function compareOutputs(expected: string, actual: string){
-//   const split1 = expected.split(EOL);
-//   const split2 = actual.split(EOL);
-//   const bigger = split1.length >= split2.length ? split1 : split2;
-//   const smaller = bigger === split1 ? split1 : split2;
-//   bigger.forEach((line, i) => {
-//     console.log("smaller[i]: ", smaller[i])
-//     if(smaller[i] !== undefined){
-//       assert(line === smaller[i])
-//     }
-//   })
-// }
+function compareOutputs(expected: string, actual: string) {
+  const split1 = expected.trim().split(EOL);
+  const split2 = actual.trim().split(EOL);
+  expect(split1.length === split2.length).toBeTruthy();
+  split1.forEach((line, i) => {
+    const trimmedLineA = line.trim().replace(/\u00A0/g, ' ');
+    const trimmedLineB = split2[i].trim().replace(/\u00A0/g, ' ');
+    expect(trimmedLineA).toEqual(trimmedLineB);
+  });
+}
