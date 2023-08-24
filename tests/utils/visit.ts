8db932308e3dd2625d8543d343d9df5d83ad15ec
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2023-08-24 14:01:02 -0600

    cleanup

--- tests/utils/visit.ts
+++ tests/utils/visit.ts
@@ -3,7 +3,6 @@ import type { Page } from '@playwright/test';
 export async function visit(page: Page, pathname: string) {
   await page.waitForTimeout(2000);
   const pageFinal = await page.goto(`${pathname}`);
-  // Using waitUntil: 'networkidle' has interminent issues
   await page.waitForTimeout(2000);
   return pageFinal;
 }
