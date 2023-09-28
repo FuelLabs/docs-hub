00420b8b044f2ae3749c349e3a0d99eb9438d2ac
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2023-09-28 09:41:06 -0600

    update to latest fuels-ts (#66)

--- docs/guides/examples/quickstart/frontend/src/contracts/index.ts
+++ docs/guides/examples/quickstart/frontend/src/contracts/index.ts
@@ -4,9 +4,9 @@
 /* eslint-disable */
 
 /*
-  Fuels version: 0.57.0
+  Fuels version: 0.60.0
   Forc version: 0.44.0
-  Fuel-Core version: 0.20.4
+  Fuel-Core version: 0.20.5
 */
 
 export type { CounterContractAbi } from './CounterContractAbi';
