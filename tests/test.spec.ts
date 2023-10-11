357d725cabaf4d1d121c27a0d46b1f99b75911de
Call Delegation <calldelegation@gmail.com>
2023-10-11 11:12:39 -0400

    Pausing for now

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -24,4 +24,51 @@ test.describe('Guides', () => {
     stopServers();
     context.close();
   });
+
+  // test('intro to sway', async ({ context, extensionId, page }) => {
+  //   const PREREQUISITES_PAGE_URL = 'guides/intro-to-sway/prerequisites';
+  //   const IMPORTS_PAGE_URL = 'guides/intro-to-sway/contract-imports';
+  //   const STRUCTS_PAGE_URL = 'guides/intro-to-sway/contract-structs';
+  //   const ABI_PAGE_URL = 'guides/intro-to-sway/contract-abi';
+  //   const STORAGE_PAGE_URL = 'guides/intro-to-sway/contract-storage';
+  //   const ERRORS_PAGE_URL = 'guides/intro-to-sway/contract-errors';
+  //   const FUNCTIONS_PAGE_URL = 'guides/intro-to-sway/contract-functions';
+  //   const CHECKPOINT_PAGE_URL = 'guides/intro-to-sway/checkpoint';
+  //   const FUELS_RS_PAGE_URL = 'guides/intro-to-sway/rust-sdk';
+  //   const FUELS_TS_PAGE_URL = 'guides/intro-to-sway/typescript-sdk';
+
+  //   // SETUP
+  //   stopServers();
+  //   await useFuelWallet(context, extensionId, page);
+  //   await setupFolders('fuel-project');
+  //   await startServers(page);
+
+  //   // TEST CONTRACT
+  //   await runTest(page, context, PREREQUISITES_PAGE_URL);
+
+  //   await runTest(page, context, IMPORTS_PAGE_URL);
+
+  //   await runTest(page, context, STRUCTS_PAGE_URL);
+
+  //   await runTest(page, context, ABI_PAGE_URL);
+
+  //   await runTest(page, context, STORAGE_PAGE_URL);
+
+  //   await runTest(page, context, ERRORS_PAGE_URL);
+
+  //   await runTest(page, context, FUNCTIONS_PAGE_URL);
+
+  //   await runTest(page, context, CHECKPOINT_PAGE_URL);
+
+  //   // await runTest(page, context, FUELS_RS_PAGE_URL);
+
+  //   await runTest(page, context, FUELS_TS_PAGE_URL);
+
+  //   // TEST FRONTEND
+  //   // await runTest(page, context, FRONTEND_PAGE_URL);
+
+  //   // SHUT DOWN
+  //   stopServers();
+  //   context.close();
+  // });
 });
