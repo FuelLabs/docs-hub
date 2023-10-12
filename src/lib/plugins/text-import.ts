b2c615692b5947c2fe7e0a78c8a197b787f8c394
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2023-10-12 08:45:33 -0600

    feat: add text imports (#79)

---
+++ src/lib/plugins/text-import.ts
@@ -0,0 +1,124 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import fs from 'node:fs';
+import { EOL } from 'os';
+import path from 'path';
+import { remark } from 'remark';
+import type { Root } from 'remark-gfm';
+import { visit } from 'unist-util-visit';
+import type { Parent } from 'unist-util-visit/lib';
+
+export type CommentTypes = '<!--' | '{/*' | '//' | '/*';
+
+export function getEndCommentType(commentType: string) {
+  let commentEnd;
+  switch (commentType) {
+    case '/*':
+      commentEnd = '*/';
+      break;
+    case '{/*':
+      commentEnd = '*/}';
+      break;
+    case '//':
+      commentEnd = '';
+      break;
+    case '<!--':
+      commentEnd = '-->';
+      break;
+    default:
+  }
+  return commentEnd;
+}
+
+function extractCommentBlock(
+  content: string,
+  comment: string,
+  commentType: CommentTypes
+) {
+  const lines = content.split(EOL);
+  const commentEnd = getEndCommentType(commentType);
+  let lineStart = 1;
+  let lineEnd = 1;
+  for (let i = 0; i < lines.length; i++) {
+    const start =
+      lines[i].replace(/\s+/g, '') ===
+      `${commentType}${comment}:example:start${commentEnd}`;
+    if (start === true) {
+      lineStart = i + 1;
+    } else {
+      const end =
+        lines[i].replace(/\s+/g, '') ===
+        `${commentType}${comment}:example:end${commentEnd}`;
+      if (end === true) {
+        lineEnd = i;
+      }
+    }
+  }
+
+  if (lineStart < 0) {
+    lineStart = 0;
+  }
+  if (lineEnd < 0) {
+    lineEnd = lines.length;
+  }
+
+  const linesContent = lines.slice(lineStart, lineEnd).join('\n');
+
+  return linesContent;
+}
+
+interface Options {
+  filepath: string;
+}
+
+export function textImport(options: Options = { filepath: '' }) {
+  const rootDir = process.cwd();
+  const { filepath } = options;
+  const dirname = path.relative(rootDir, path.dirname(filepath));
+
+  return function transformer(tree: Root) {
+    const nodes: [any, number | null, Parent<any, any>][] = [];
+
+    visit(tree, 'mdxJsxFlowElement', (node: any, idx, parent) => {
+      if (node.name === 'TextImport') {
+        nodes.push([node as any, idx ?? null, parent as Parent<any, any>]);
+      }
+    });
+
+    nodes.forEach(([node]) => {
+      const attr = node.attributes;
+      let content = '';
+
+      if (!attr.length) {
+        throw new Error('TextImport needs to have properties defined');
+      }
+
+      const file = attr.find((i: any) => i.name === 'file')?.value;
+      const comment = attr.find((i: any) => i.name === 'comment')?.value;
+      const commentType = attr.find(
+        (i: any) => i.name === 'commentType'
+      )?.value;
+      let linesIncluded =
+        attr.find((i: any) => i.name === 'linesIncluded')?.value || [];
+      const fileAbsPath = path.resolve(path.join(rootDir, dirname), file);
+      const fileContent = fs.readFileSync(fileAbsPath, 'utf8');
+      const resp = Array.isArray(linesIncluded) ? 0 : linesIncluded;
+      if (resp !== 0) {
+        linesIncluded = JSON.parse(linesIncluded.value);
+      }
+      const commentResult = extractCommentBlock(
+        fileContent,
+        comment,
+        commentType
+      );
+      content = commentResult;
+
+      const processor = remark();
+      const ast = processor.parse(content);
+
+      node.type = 'element';
+      node.children = ast.children;
+      node.tagName = 'div';
+    });
+  };
+}
