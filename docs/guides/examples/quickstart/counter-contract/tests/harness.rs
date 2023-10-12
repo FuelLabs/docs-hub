44c657cd2e62e68ebee45c6db6aa64dbd15a26ae
Call Delegation <106365423+calldelegation@users.noreply.github.com>
2023-10-12 10:27:36 -0400

    docs: Fixing quickstart tests (#78)
    * Resolving new example
    
    * Bump to v0.48.0
    
    * Formatting
    
    * Fixing formatting before committing again
    
    * Generating contracts temp
    
    * Generating contracts temp
    
    * formatting
    
    * Ready to merge once sway file has been updated

--- docs/guides/examples/quickstart/counter-contract/tests/harness.rs
+++ docs/guides/examples/quickstart/counter-contract/tests/harness.rs
@@ -21,16 +21,14 @@ async fn get_contract_instance() -> (MyContract<WalletUnlocked>, ContractId) {
     .await;
     let wallet = wallets.pop().unwrap();
 
-    let storage_config =
-        StorageConfiguration::load_from("out/debug/counter-contract-storage_slots.json").unwrap();
-
-    let load_config = LoadConfiguration::default().with_storage_configuration(storage_config);
-
-    let id = Contract::load_from("./out/debug/counter-contract.bin", load_config)
-        .unwrap()
-        .deploy(&wallet, TxParameters::default())
-        .await
-        .unwrap();
+    let id = Contract::load_from(
+        "./out/debug/counter-contract.bin",
+        LoadConfiguration::default(),
+    )
+    .unwrap()
+    .deploy(&wallet, TxParameters::default())
+    .await
+    .unwrap();
 
     let instance = MyContract::new(id.clone(), wallet);
 
