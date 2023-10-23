82acdbd1b00389cb757d52f2438137e6b6ec64e9
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2023-10-23 11:13:58 -0600

    test

--- scripts/generate-mlc-config.mjs
+++ scripts/generate-mlc-config.mjs
@@ -1,12 +1,14 @@
-import fs from 'fs';
+// import fs from 'fs';
 
 main();
 
 function main() {
-  const previewURL = process.argv[2];
-  console.log('PREVIEW URL:', previewURL);
-  const configPath = './mlc-config-test.json';
-  const configFile = fs.readFileSync(configPath, 'utf-8');
-  const newContent = configFile.replace('{{VERCEL_PREVIEW}}', previewURL);
-  fs.writeFileSync(configPath, newContent);
+  console.log('DEPLOYMENT STATE:', process.argv[2]);
+  console.log('DEPLOYMENT URL:', process.argv[3]);
+  //   const previewURL = process.argv[2];
+  //   console.log('PREVIEW URL:', previewURL);
+  //   const configPath = './mlc-config-test.json';
+  //   const configFile = fs.readFileSync(configPath, 'utf-8');
+  //   const newContent = configFile.replace('{{VERCEL_PREVIEW}}', previewURL);
+  //   fs.writeFileSync(configPath, newContent);
 }
