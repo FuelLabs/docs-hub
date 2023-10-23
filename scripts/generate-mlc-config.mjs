f8de6d3dcc07257bb3569f30dd369a8323ace80b
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2023-10-23 11:26:53 -0600

    cleanup

--- scripts/generate-mlc-config.mjs
+++ scripts/generate-mlc-config.mjs
@@ -1,14 +1,20 @@
-// import fs from 'fs';
+import fs from 'fs';
 
 main();
 
+const configPath = './mlc-config-test.json';
+
 function main() {
-  console.log('DEPLOYMENT STATE:', process.argv[2]);
-  console.log('DEPLOYMENT URL:', process.argv[3]);
-  //   const previewURL = process.argv[2];
-  //   console.log('PREVIEW URL:', previewURL);
-  //   const configPath = './mlc-config-test.json';
-  //   const configFile = fs.readFileSync(configPath, 'utf-8');
-  //   const newContent = configFile.replace('{{VERCEL_PREVIEW}}', previewURL);
-  //   fs.writeFileSync(configPath, newContent);
+  const deploymentState = process.argv[2];
+  console.log('DEPLOYMENT STATE:', deploymentState);
+  const deploymentURL = process.argv[3];
+  console.log('DEPLOYMENT URL:', deploymentURL);
+
+  if (deploymentState === 'success' && deploymentURL) {
+    const configFile = fs.readFileSync(configPath, 'utf-8');
+    const newContent = configFile.replace('{{VERCEL_PREVIEW}}', deploymentURL);
+    fs.writeFileSync(configPath, newContent);
+  } else {
+    throw Error('MISSING VERCEL DEPLOYMENT');
+  }
 }
