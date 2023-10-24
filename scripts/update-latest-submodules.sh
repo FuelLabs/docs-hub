59a9b296a2b643cc5cc124db372ee88ce5f094e6
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2023-10-24 08:58:14 -0600

    ci: lint & check spelling/links in guides (#91)

--- scripts/update-latest-submodules.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-git submodule update --remote docs/latest/sway docs/latest/builds/sway docs/latest/fuel-graphql-docs docs/latest/fuel-indexer docs/latest/fuel-specs docs/latest/fuels-rs docs/latest/fuels-ts docs/latest/fuels-wallet docs/latest/fuelup
