96fe26ea9f5b3aa81c3a4afcd73034b63c4cef27
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2023-11-08 13:51:31 -0700

    fix sway examples (#106)

--- scripts/clean-build-files.sh
+++ scripts/clean-build-files.sh
@@ -19,7 +19,7 @@ TARGET_DIRS=(
 )
 
 # File/folder names to exclude from deletion in each book
-EXCLUSIONS_sway=("Cargo.toml" "forc-pkg" "docs" "examples" "master")
+EXCLUSIONS_sway=("Cargo.toml" "forc-pkg" "sway-lib-std" "docs" "examples" "master")
 EXCLUSIONS_fuels_rs=("Cargo.toml" "docs" "examples" "packages")
 EXCLUSIONS_fuels_ts=("apps" "packages" "package.json")
 EXCLUSIONS_fuelup=("Cargo.toml" "docs")
