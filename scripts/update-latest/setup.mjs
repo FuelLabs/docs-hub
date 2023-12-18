86f2b9f22bfc89573e4d34fca216bf2eb40190b1
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2023-12-18 10:06:52 -0700

    cleanup

--- scripts/update-latest/setup.mjs
+++ scripts/update-latest/setup.mjs
@@ -1,11 +1,6 @@
-import { updateSubmodule, setupUser } from './gitUtils.mjs';
+import { setupUser } from './gitUtils.mjs';
 
 export async function setup() {
   // setup git
   await setupUser();
-
-  // checkout the nightly gh-pages branch of fuelup
-  const fuelupFolder = 'docs/nightly/fuelup';
-  await updateSubmodule(fuelupFolder);
-  console.log('UPDATED FUELUP SUBMODLE');
 }
