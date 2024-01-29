a5a1e4bec44d377c10a3174b8571da2db8cf0947
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2024-01-29 15:30:13 -0700

    add FuelProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/index.css';
 import '../styles/docsearch.css';
+import { FuelProvider } from '@fuel-wallet/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
@@ -11,20 +12,22 @@ const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <VersionProvider>
-        <Provider>
-          <style jsx global>{`
-            :root {
-              --fonts-sans: system-ui, 'Inter', sans-serif;
-              --fonts-display: system-ui, 'Inter', sans-serif;
-              --fonts-mono: 'Inconsolata';
-            }
-          `}</style>
-          <Component {...pageProps} />
-          <Analytics />
-        </Provider>
-      </VersionProvider>
-    </QueryClientProvider>
+    <FuelProvider>
+      <QueryClientProvider client={queryClient}>
+        <VersionProvider>
+          <Provider>
+            <style jsx global>{`
+              :root {
+                --fonts-sans: system-ui, 'Inter', sans-serif;
+                --fonts-display: system-ui, 'Inter', sans-serif;
+                --fonts-mono: 'Inconsolata';
+              }
+            `}</style>
+            <Component {...pageProps} />
+            <Analytics />
+          </Provider>
+        </VersionProvider>
+      </QueryClientProvider>
+    </FuelProvider>
   );
 }
