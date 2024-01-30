0fff819b50ea052ab2f10abddea4b0aaf5e68c95
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2024-01-30 09:12:35 -0700

    fix

--- docs/guides/examples/quickstart/frontend/src/index.tsx
+++ docs/guides/examples/quickstart/frontend/src/index.tsx
@@ -10,7 +10,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <FuelProvider>
+    <FuelProvider 
+      fuelConfig={{
+          devMode: true,
+        }}
+      >
       <App />
     </FuelProvider>
   </React.StrictMode>
