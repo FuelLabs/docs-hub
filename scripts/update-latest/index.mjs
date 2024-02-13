cea46975bc55a9641b698f59f3a183c5adff3836
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2024-02-13 15:50:38 -0700

    start updating update script

--- scripts/update-latest/index.mjs
+++ scripts/update-latest/index.mjs
@@ -1,13 +1,18 @@
 import { checkIfNightlyIsNew } from './checkNightly.mjs';
 import { setupUser } from './gitUtils.mjs';
-import { updateNightly } from './updateNightly.mjs';
+// import { updateNightly } from './updateNightly.mjs';
 
 main();
 
 async function main() {
-  await setupUser();
-  console.log('GETTING NEW VERSIONS');
-  const newVersions = await checkIfNightlyIsNew();
-  console.log('GOT NEW VERSIONS', newVersions);
-  await updateNightly(newVersions);
+  if (process.argv.includes('--setup-git-user')) {
+    await setupUser();
+  }
+
+  if (process.argv.includes('--nightly')) {
+    console.log('GETTING NEW VERSIONS');
+    const newVersions = await checkIfNightlyIsNew();
+    console.log('GOT NEW VERSIONS', newVersions);
+    // await updateNightly(newVersions);
+  }
 }
