f31b5f6737018d924934dd7cfa9913cc9bd37359
Matt Auer <mattauer@umich.edu>
2024-04-03 16:55:39 -0500

    feat: improve markdown

--- scripts/fetch-release-notes/fetchReleaseNotes.mjs
+++ scripts/fetch-release-notes/fetchReleaseNotes.mjs
@@ -2,29 +2,43 @@ import fs from "fs";
 import dotenv from "dotenv";
 dotenv.config();
 
-const REPOS = ["sway", "fuel-core", "fuels-rs", "fuels-ts", "fuels-wallet", "block-explorer-v2"]
+const REPOS = [
+  { repoName: "sway", docName: "Sway" },
+  { repoName: "fuel-core", docName: "Fuel Core" },
+  { repoName: "fuels-rs", docName: "Rust SDK" },
+  { repoName: "fuels-ts", docName: "TS SDK" },
+  { repoName: "fuels-wallet", docName: "Fuel Wallet" },
+  { repoName: "block-explorer-v2", docName: "Fuel Explorer" },
+];
+
+const re = new RegExp("#+ ", "g");
 
 export async function fetchReleaseNotes() {
-    let releaseNoteContent = "";
-    for (const repo of REPOS) {
-        const url = `https://api.github.com/repos/FuelLabs/${repo}/releases/latest`;
-        const response = await fetch(url, {
-            credentials: "include",
-            headers: {
-                "Accept": "application/vnd.github+json",
-                "Authorization": `Bearer ${process.env.GITHUB_AUTH}`,
-                "X-GitHub-Api-Version": "2022-11-28"
-            },
-            redirect: "follow"
-        });
-        const responseJson = await response.json();
-        const releaseNotes = responseJson.body.replaceAll("<", "\\<").replaceAll(">", "\\>");
-        const releaseName = responseJson.name;
-        releaseNoteContent += `## ${repo}
-        ## ${releaseName}
+  let releaseNoteContent = "";
+  for (const repo of REPOS) {
+    const url = `https://api.github.com/repos/FuelLabs/${repo.repoName}/releases/latest`;
+    const response = await fetch(url, {
+      credentials: "include",
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${process.env.GITHUB_AUTH}`,
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+      redirect: "follow",
+    });
+    const responseJson = await response.json();
+    const releaseNotes = responseJson.body
+      .replaceAll("<", "\\<")
+      .replaceAll(">", "\\>")
+      .replaceAll(re, (match) => {
+        return `#${match}`;
+      });
+    const releaseName = responseJson.name;
+    releaseNoteContent += `## ${repo.docName}
+        ### ${releaseName}
         ${releaseNotes}]\n`;
-    }
-    let content = `---
+  }
+  let content = `---
 title: Release Notes and Changelogs
 category: Understanding Fuel
 ---
@@ -32,8 +46,8 @@ category: Understanding Fuel
 # Release Notes and Changelogs
 
 `;
-    content += releaseNoteContent;
-    console.log(content);
+  content += releaseNoteContent;
+  console.log(content);
 
-    fs.writeFileSync("./docs/fuel-101/releasenotes-changelogs.mdx", content);
+  fs.writeFileSync("./docs/fuel-101/releasenotes-changelogs.mdx", content);
 }
