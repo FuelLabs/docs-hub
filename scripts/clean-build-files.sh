df272919e9f15fd5c1d1ec13e0435abfb2cc8c68
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2024-05-09 11:41:13 -0600

    try fix

--- scripts/clean-build-files.sh
+++ scripts/clean-build-files.sh
@@ -54,3 +54,46 @@ for TARGET_DIR in "${TARGET_DIRS[@]}"; do
     # Return to the original directory, popping from directory stack
     popd > /dev/null
 done
+
+
+TARGET_SUB_DIRS=(
+"./docs/sway/test/src" 
+)
+
+EXCLUSIONS_test=("sdk-harness")
+
+
+for TARGET_DIR in "${TARGET_SUB_DIRS[@]}"; do
+    # Check if directory exists
+    if [[ ! -d "$TARGET_DIR" ]]; then
+        echo "Warning: Target directory $TARGET_DIR does not exist. Skipping..."
+        continue
+    fi
+
+    # Change to the target directory, pushing to directory stack
+    pushd "$TARGET_DIR" > /dev/null
+
+    for item in *; do
+        should_delete=true
+
+       dir_basename=$(basename "$TARGET_DIR" | tr '-' '_')
+       current_exclusions="EXCLUSIONS_$dir_basename[@]"
+
+        for exclusion in "${!current_exclusions}"; do
+            if [[ "$item" == "$exclusion" ]]; then
+                should_delete=false
+                break
+            fi
+        done
+
+        if $should_delete; then
+            rm -rf "$item"
+            # echo "DELETED: $item"
+        fi
+    done
+
+    echo "Cleanup done for $TARGET_DIR!"
+
+    # Return to the original directory, popping from directory stack
+    popd > /dev/null
+done
