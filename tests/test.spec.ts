c5d72af33ee5e08601856c06486c12f8b2a9bc32
Call Delegation <106365423+calldelegation@users.noreply.github.com>
2024-06-07 14:49:26 -0400

    docs: Fix command (#310)
    * temp adjustment of contract quickstart
    
    * test
    
    * lint
    
    * ci

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,17 +1,34 @@
-import { test } from './utils/fixtures';
-import { runTest } from './utils/runTest';
-import { setupFolders, startServers, stopServers } from './utils/setup';
-import { useFuelWallet } from './utils/wallet';
+import { test } from "./utils/fixtures";
+import { runTest } from "./utils/runTest";
+import { setupFolders, startServers, stopServers } from "./utils/setup";
+import { useFuelWallet } from "./utils/wallet";
 
-test.describe('Guides', () => {
-  test('counter-dapp', async ({ context, extensionId, page }) => {
-    const CONTRACT_PAGE_URL = 'guides/counter-dapp/building-a-smart-contract';
-    const FRONTEND_PAGE_URL = 'guides/counter-dapp/building-a-frontend';
+test.describe("Guides", () => {
+  test("quickstart", async ({ context, extensionId, page }) => {
+    const CONTRACT_QUICKSTART_PAGE_URL = "guides/contract-quickstart";
 
     // SETUP
     stopServers();
     await useFuelWallet(context, extensionId, page);
-    await setupFolders('fuel-project');
+    await setupFolders("fuel-project");
+    await startServers(page);
+
+    // TEST CONTRACT QUICKSTART
+    await runTest(page, context, CONTRACT_QUICKSTART_PAGE_URL);
+
+    // SHUT DOWN
+    stopServers();
+    // context.close();
+  });
+
+  test("counter-dapp", async ({ context, extensionId, page }) => {
+    const CONTRACT_PAGE_URL = "guides/counter-dapp/building-a-smart-contract";
+    const FRONTEND_PAGE_URL = "guides/counter-dapp/building-a-frontend";
+
+    // SETUP
+    stopServers();
+    await useFuelWallet(context, extensionId, page);
+    await setupFolders("fuel-project");
     await startServers(page);
 
     // TEST CONTRACT
@@ -25,22 +42,22 @@ test.describe('Guides', () => {
     // context.close();
   });
 
-  test('intro to sway', async ({ context, extensionId, page }) => {
-    const PREREQUISITES_PAGE_URL = 'guides/intro-to-sway/prerequisites';
-    const IMPORTS_PAGE_URL = 'guides/intro-to-sway/contract-imports';
-    const STRUCTS_PAGE_URL = 'guides/intro-to-sway/contract-structs';
-    const ABI_PAGE_URL = 'guides/intro-to-sway/contract-abi';
-    const STORAGE_PAGE_URL = 'guides/intro-to-sway/contract-storage';
-    const ERRORS_PAGE_URL = 'guides/intro-to-sway/contract-errors';
-    const FUNCTIONS_PAGE_URL = 'guides/intro-to-sway/contract-functions';
-    const CHECKPOINT_PAGE_URL = 'guides/intro-to-sway/checkpoint';
-    const FUELS_RS_PAGE_URL = 'guides/intro-to-sway/rust-sdk';
-    const FUELS_TS_PAGE_URL = 'guides/intro-to-sway/typescript-sdk';
+  test("intro to sway", async ({ context, extensionId, page }) => {
+    const PREREQUISITES_PAGE_URL = "guides/intro-to-sway/prerequisites";
+    const IMPORTS_PAGE_URL = "guides/intro-to-sway/contract-imports";
+    const STRUCTS_PAGE_URL = "guides/intro-to-sway/contract-structs";
+    const ABI_PAGE_URL = "guides/intro-to-sway/contract-abi";
+    const STORAGE_PAGE_URL = "guides/intro-to-sway/contract-storage";
+    const ERRORS_PAGE_URL = "guides/intro-to-sway/contract-errors";
+    const FUNCTIONS_PAGE_URL = "guides/intro-to-sway/contract-functions";
+    const CHECKPOINT_PAGE_URL = "guides/intro-to-sway/checkpoint";
+    const FUELS_RS_PAGE_URL = "guides/intro-to-sway/rust-sdk";
+    const FUELS_TS_PAGE_URL = "guides/intro-to-sway/typescript-sdk";
 
     // SETUP
     stopServers();
     await useFuelWallet(context, extensionId, page);
-    await setupFolders('fuel-project');
+    await setupFolders("fuel-project");
     await startServers(page);
 
     // TEST CONTRACT
@@ -60,25 +77,25 @@ test.describe('Guides', () => {
     // context.close();
   });
 
-  test('intro to predicates', async ({ context, extensionId, page }) => {
-    const PREREQUISITES_PAGE_URL = 'guides/intro-to-predicates/prerequisites';
-    const PREDICATE_ROOT_PAGE_URL = 'guides/intro-to-predicates/predicate-root';
-    const IMPORTS_PAGE_URL = 'guides/intro-to-predicates/imports';
-    const CONFIGURABLES_PAGE_URL = 'guides/intro-to-predicates/configurables';
+  test("intro to predicates", async ({ context, extensionId, page }) => {
+    const PREREQUISITES_PAGE_URL = "guides/intro-to-predicates/prerequisites";
+    const PREDICATE_ROOT_PAGE_URL = "guides/intro-to-predicates/predicate-root";
+    const IMPORTS_PAGE_URL = "guides/intro-to-predicates/imports";
+    const CONFIGURABLES_PAGE_URL = "guides/intro-to-predicates/configurables";
     const SIGNATURE_VERIFICATION_PAGE_URL =
-      'guides/intro-to-predicates/signature-verification';
-    const MAIN_PAGE_URL = 'guides/intro-to-predicates/main';
-    const CHECKPOINT_PAGE_URL = 'guides/intro-to-predicates/checkpoint';
+      "guides/intro-to-predicates/signature-verification";
+    const MAIN_PAGE_URL = "guides/intro-to-predicates/main";
+    const CHECKPOINT_PAGE_URL = "guides/intro-to-predicates/checkpoint";
     const SCRIPT_DEBUG_PAGE_URL =
-      'guides/intro-to-predicates/debugging-with-scripts';
+      "guides/intro-to-predicates/debugging-with-scripts";
     const SCRIPT_LOGS_PAGE_URL =
-      'guides/intro-to-predicates/debugging-with-scripts-rust';
-    const FUELS_RS_PAGE_URL = 'guides/intro-to-predicates/rust-sdk';
+      "guides/intro-to-predicates/debugging-with-scripts-rust";
+    const FUELS_RS_PAGE_URL = "guides/intro-to-predicates/rust-sdk";
 
     // SETUP
     stopServers();
     await useFuelWallet(context, extensionId, page);
-    await setupFolders('fuel-project');
+    await setupFolders("fuel-project");
     await startServers(page);
 
     // TEST CONTRACT
