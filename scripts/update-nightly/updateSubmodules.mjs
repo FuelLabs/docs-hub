49d2d3b27b011d6f83af60085e963669646ff613
Call Delegation <106365423+calldelegation@users.noreply.github.com>
2024-07-02 15:23:36 -0700

    nits

--- scripts/update-nightly/updateSubmodules.mjs
+++ scripts/update-nightly/updateSubmodules.mjs
@@ -34,7 +34,6 @@ export async function updateSubmodules(newDefaultVersions, newNightlyVersions) {
   const updateRegardless = [
     'docs/nightly/fuel-specs',
     'docs/nightly/fuel-graphql-docs',
-    'docs/guides/docs/migration-guide/migrations-and-disclosures',
   ];
   await Promise.all(
     updateRegardless.map(async (sub) => {
