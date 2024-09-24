168fa394a44885422aebd1012abd396881aee360
Matt <54373384+matt-user@users.noreply.github.com>
2024-09-24 15:15:51 -0500

    feat: add integration docs (#432)
    * added integration docs
    
    * add integrations
    
    * fix path
    
    * add nightly integration docs
    
    * fix: links

--- scripts/generate-links/getDocs.mjs
+++ scripts/generate-links/getDocs.mjs
@@ -100,6 +100,24 @@ export async function getDocs(key, order) {
         '!**/SUMMARY.md',
       ];
       break;
+    case 'integration-docs':
+      paths = [
+        // INTEGRATION DOCS
+        './integration-docs/docs/src/**/*.md',
+        './integration-docs/docs/src/*.md',
+        // IGNORE ALL SUMMARY PAGES
+        '!**/SUMMARY.md',
+      ];
+      break;
+    case 'nightly-integration-docs':
+      paths = [
+        // INTEGRATION DOCS
+        './nightly/integration-docs/docs/src/**/*.md',
+        './nightly/integration-docs/docs/src/*.md',
+        // IGNORE ALL SUMMARY PAGES
+        '!**/SUMMARY.md',
+      ];
+      break;
     case 'forc':
       paths = [
         // FORC DOCS
@@ -197,7 +215,7 @@ export async function getDocs(key, order) {
     case 'guides':
       paths = [
         // GUIDES
-        './guides/docs/**/*.mdx'
+        './guides/docs/**/*.mdx',
       ];
       break;
     case 'intro':
@@ -242,6 +260,7 @@ export async function getDocs(key, order) {
       path,
     };
   });
+
   return final;
 }
 
