575f4f497506192dfd5422eac70282047889f697
Peter Smith <peter@blueoceancomputing.co.uk>
2024-11-25 12:01:26 +0000

    chore: implemented relative path parsing for TS docs (#505)
    * chore: implemented relative path parsing for TS docs
    
    * chore: fixing issue for TS docs
    
    * chore: fix spell check
    
    * chore: updated fuels-ts commit to new docs
    
    * Revert "chore: updated fuels-ts commit to new docs"
    
    This reverts commit c9207a25718aafbf0399a26adec3e768fad7a577.
    
    * Update src/lib/plugins/mdbook-example-import.ts
    
    Co-authored-by: Anderson Arboleya <anderson@arboleya.me>
    
    ---------
    
    Co-authored-by: Anderson Arboleya <anderson@arboleya.me>

--- src/lib/plugins/mdbook-example-import.ts
+++ src/lib/plugins/mdbook-example-import.ts
@@ -90,14 +90,9 @@ export function handleExampleImports(
     if (paths.length > 1) exampleName = filePath.split(':').pop();
   } else if (node.type === 'text') {
     // handle ts-sdk docs example format
-    filePath = filePath.replace('<<< @/', '').replace('<<< @', '');
+    const relativePath = dirname.replace('docs/fuels-ts/', '')
+    filePath = filePath.replace('<<< @./', `${relativePath}/`).replace(/<<< @\/?/, '');
 
-    if (
-      filePath.startsWith('docs-snippets') ||
-      filePath.startsWith('demo-fuels')
-    ) {
-      filePath = `apps/${filePath}`;
-    }
     const pathData = filePath.split('{');
     filePath = pathData[0];
 
@@ -134,7 +129,8 @@ export function handleExampleImports(
         .replace(
           'fuels-ts/create-fuels-counter-guide',
           'fuels-ts/apps/create-fuels-counter-guide'
-        );
+        )
+        .replace('fuels-ts/docs', 'fuels-ts/apps/docs');
     }
     const fileContent = fs.readFileSync(fileAbsPath, 'utf8');
     const cachedFile = getFilesOnCache(fileAbsPath);
